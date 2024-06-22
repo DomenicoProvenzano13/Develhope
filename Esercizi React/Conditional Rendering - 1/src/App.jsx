84184797674fd8e86e26a2228cc0c5ef9f679746
DomenicoProvenzano13 <domenicoprovenzano13@yahoo.it>
2024-06-22 17:04:02 +0200

    esercizio Context svolto

--- Esercizi React/Conditional Rendering - 1/src/App.jsx
+++ Esercizi React/Conditional Rendering - 1/src/App.jsx
@@ -10,39 +10,54 @@ import FocusableInput from "./components/FocusableInput";
 import { Colors } from "./components/Colors";
 import { TodoList } from "./components/TodoList";
 import { Container } from "./components/Container";
+import { LanguageContext } from "./components/LanguageContext";
+import { useState } from "react";
 
-export default function App() {
+export function App() {
+  const [language, setLanguage] = useState("it");
+
+  function handleSetLanguage(language) {
+    setLanguage(language);
+  }
+
   return (
-    <Container title={<h1>My new App</h1>}>
-      <hr />
-      <Welcome name="John" age={45} />
-      <hr />
-      <AlertClock />
-      <hr />
-      <HelloWorld />
-      <hr />
-      <Counter initialValue={42} />
-      <hr />
-      <Clock />
-      <hr />
-      <MouseClicker />
-      <hr />
-      <ControlledForm />
-      <hr />
-      <UncontrolledForm />
-      <hr />
-      <FocusableInput />
-      <hr />
-      <Colors
-        colors={[
-          { id: 1, name: "Blue" },
-          { id: 2, name: "Red" },
-          { id: 3, name: "Green" },
-        ]}
-      />
-      <hr />
-      <TodoList />
-      <hr />
-    </Container>
+    <div>
+      <button onClick={() => handleSetLanguage("en")}>ENG</button>
+      <button onClick={() => handleSetLanguage("it")}>ITA</button>
+
+      <Container title={<h1>My new App</h1>}>
+        <LanguageContext.Provider value={language}>
+          <hr />
+          <Welcome name="John" age={45} />
+          <hr />
+          <AlertClock />
+          <hr />
+          <HelloWorld />
+          <hr />
+          <Counter initialValue={42} />
+          <hr />
+          <Clock />
+          <hr />
+          <MouseClicker />
+          <hr />
+          <ControlledForm />
+          <hr />
+          <UncontrolledForm />
+          <hr />
+          <FocusableInput />
+          <hr />
+          <Colors
+            colors={[
+              { id: 1, name: "Blue" },
+              { id: 2, name: "Red" },
+              { id: 3, name: "Green" },
+            ]}
+          />
+          <hr />
+          <TodoList />
+          <hr />
+        </LanguageContext.Provider>
+      </Container>
+    </div>
   );
 }
--- Esercizi React/Conditional Rendering - 1/src/components/Clock.jsx
+++ Esercizi React/Conditional Rendering - 1/src/components/Clock.jsx
@@ -1,16 +1,24 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { LanguageContext } from "./LanguageContext";
 
 export function Clock() {
   const [date, setDate] = useState(new Date());
 
+  const language = useContext(LanguageContext);
+
   useEffect(() => {
+    console.log("Clock component mounted");
     setInterval(() => {
       setDate(new Date());
     }, 1000);
   }, []);
   return (
     <div>
-      <h2>Sono le ore: {date.toLocaleTimeString()}</h2>
+      <h2>
+        {language === "it"
+          ? `Sono le ore: ${date.toLocaleTimeString()}`
+          : `It's ${date.toLocaleTimeString()}`}
+      </h2>
     </div>
   );
 }
