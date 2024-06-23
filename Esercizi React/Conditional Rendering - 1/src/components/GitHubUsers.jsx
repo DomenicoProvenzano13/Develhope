91fee943ebbb456f52c6ecccc6553d881cf646e9
DomenicoProvenzano13 <domenicoprovenzano13@yahoo.it>
2024-06-23 21:03:57 +0200

    Custom Hooks - 3 Svolto

--- Esercizi React/Conditional Rendering - 1/src/components/GitHubUsers.jsx
+++ Esercizi React/Conditional Rendering - 1/src/components/GitHubUsers.jsx
@@ -1,35 +1,15 @@
-import { useEffect, useState } from "react";
+import { useGithubUser } from "./hooks/useGithubUser";
 
 export function GithubUser({ username }) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { data, loading, error, onFetchUser } = useGithubUser(username);
 
-  async function fetchGithubUser(username) {
-    setLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      const json = await response.json();
-      if (response.status !== 200) {
-        setError(new Error("The has been an error!"));
-      }
-      setData(json);
-    } catch (error) {
-      setError(error);
-      setData(null);
-    } finally {
-      setLoading(false);
-    }
+  function handleGetUserData() {
+    onFetchUser(username);
   }
-
-  useEffect(() => {
-    fetchGithubUser(username);
-  }, [username]);
 
   return (
     <div>
+      <button onClick={handleGetUserData}>Load user data</button>
       {loading && <h1>Loading...</h1>}
       {error && <h2>{error.message}</h2>}
       {data && (
--- Esercizi React/Conditional Rendering - 1/src/components/SrcGithubUsers.jsx
+++ Esercizi React/Conditional Rendering - 1/src/components/SrcGithubUsers.jsx
@@ -1,3 +1,46 @@
+/* import { useEffect, useState } from "react";
+
+export function SrcGithubUsers(username) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchGithubUser(username) {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      setError(error);
+      setData(null);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchGithubUser(username);
+  }, [username]);
+
+  function handleSrcGithubUser(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSrcGithubUser}>
+        <input name="Search"></input>
+      </form>
+      {data && <h1>{data.name}</h1>}
+      {data && <h2>{data.login}</h2>}
+      {data && <img src={data.avatar_url} style={{ width: "50px" }}></img>}
+    </div>
+  );
+} */
+
 import { useRef, useState } from "react";
 import { GithubUser } from "./GitHubUsers";
 
